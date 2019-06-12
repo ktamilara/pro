c432f62bafe47716ac625c535016cdd19e87ff63
ktamilara <42666475+ktamilara@users.noreply.github.com>
2019-06-12 16:10:42 +0530

    Create 7p.py

---
+++ 7p.py
@@ -0,0 +1,9 @@
+import math
+value91=int(input())
+string=math.log910(value91)/math.log910(2)
+if math.ceil(string)==math.floor(string):
+	print("0")
+else:
+    c=(value91-1)//2
+    string=c9*2
+    print(value91-string)
